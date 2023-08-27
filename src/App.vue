72133308391c3c4317b2d4bcb950b1247844757c
YerrouHamza <hamzayerrou5@yahoo.com>
2023-08-27 19:12:06 +0100

    fixed router issue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <router-view />
 </template>
 
 <script setup>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
-    <router-view />
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
 </template>
 
 <script setup>
