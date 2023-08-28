f3f9dcbaf080070b2a3a0a75d57cb8aef5b4c41f
YerrouHamza <hamzayerrou5@yahoo.com>
2023-08-28 19:50:41 +0100

    setup the storte and call it

---
+++ src/store/index.js
@@ -0,0 +1,26 @@
+import { createStore } from "vuex";
+
+
+const store = createStore({
+    state: {
+        meals: ['food']
+    },
+
+    // mutations
+    mutations: {
+
+    },
+
+    // actions
+    actions: {
+
+    },
+
+    // getters
+    getters : {
+        
+    }
+})
+
+
+export default store;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,20 @@
     <h1 class="text-3xl font-bold underline">
         Hello world!
     </h1>
+    <pre>{{ meals }}</pre>
 </template>
 
 <script setup>
+    import { computed } from 'vue';
+    import { useStore } from 'vuex';
+
+    const store = useStore();
+
+    const meals = computed(() => store.state.meals)
+
+
+    console.log(meals.value)
+
 
 
 </script>
