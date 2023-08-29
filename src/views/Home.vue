38e71471f2cd04f41b5b0656deb283acb74c3b6e
YerrouHamza <hamzayerrou5@yahoo.com>
2023-08-30 00:13:35 +0100

    created the meals cards and section previews

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,10 +15,29 @@
         </form>
     </header>
 
-    <pre>
-        {{ meals }}
-    </pre>
-    
+    <section class="lg:container mx-auto mt-7 p-4">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-6">
+            
+            <div v-for="meal in meals" :key="meal.idMeal" class="max-w-sm mx-auto md:mx-0 bg-white border border-gray-200 rounded-lg shadow">
+                <a href="#">
+                    <img class="rounded-t-lg" :src="meal.strMealThumb" :alt="meal.strMeal" />
+                </a>
+                <div class="p-5">
+                    <a href="#">
+                        <h5 class="maxLine-2 mb-2 text-2xl font-bold tracking-tight text-gray-900">{{ meal.strMeal }}</h5>
+                    </a>
+                    <p class="maxLines-3 mb-3 font-normal text-gray-700">{{ meal.strInstructions }}</p>
+                    <router-link to="" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                        Read more
+                        <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </router-link>
+                </div>
+            </div>
+
+        </div>
+    </section>
 </template>
 
 <script setup>
@@ -41,4 +60,19 @@
 </script>
 
 <style scoped>
+
+    .maxLines-2, .maxLines-3 {
+        overflow: hidden;
+        display: -webkit-box;
+    }
+    .maxLines-2 {
+        -webkit-line-clamp: 2; /* number of lines to show */
+                line-clamp: 2; 
+        -webkit-box-orient: vertical;
+    }
+    .maxLines-3 {
+        -webkit-line-clamp: 3; /* number of lines to show */
+                line-clamp: 3; 
+        -webkit-box-orient: vertical;
+    }
 </style>
