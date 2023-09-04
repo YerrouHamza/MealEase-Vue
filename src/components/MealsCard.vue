aa00e8d2b278e3c9b496f06f84cc118b87c529de
YerrouHamza <hamzayerrou5@yahoo.com>
2023-09-04 22:51:17 +0100

    make the each meal card open single meal details

--- src/components/MealsCard.vue
+++ src/components/MealsCard.vue
@@ -6,18 +6,18 @@
         <div class="p-5">
             <h5 class="maxLine-2 mb-2 text-2xl font-bold tracking-tight text-gray-900">{{ props.title }}</h5>
             <p class="maxLines-3 mb-3 font-normal text-gray-700">{{ props.discription }}</p>
-            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+            <router-link :to="{name: 'singleMeal', params: {id: props.id}}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
                 Read more
                 <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                 </svg>
-            </a>
+            </router-link>
         </div>
     </div>
 </template>
 
 <script setup>
-    const props = defineProps(['title', 'discription', 'image'])
+    const props = defineProps(['title', 'discription', 'image', 'id'])
 </script>
 
 <style scoped>
