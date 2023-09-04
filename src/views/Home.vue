4a972d5119e6db67f5e65f0cf2e86b37cfb26940
YerrouHamza <hamzayerrou5@yahoo.com>
2023-09-04 20:11:59 +0100

    move the meals card to component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="h-[40vh] p-4 flex flex-col justify-center items-center bg-gradient-to-r from-sky-500 to-indigo-500">
         <h1 class="text-4xl font-semibold text-gray-100 mb-8">Find Your Meals For <span class="font-bold uppercase underline">free</span></h1>
-        <form>   
+        <form @submit.prevent="searchMealsByName">   
             <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
             <div class="relative">
                 <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -9,49 +9,36 @@
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                     </svg>
                 </div>
-                <input type="text" v-model="mealByname" class="block max-w-[90vw] w-[500px] p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search meals by name" required>
-                <button type="button" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2" @click="searchMealsByName">Search</button>
+                <input type="text" v-model="mealByname" class="block max-w-[90vw] w-[500px] p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search meals by name">
+                <button type='button' class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2" @click="searchMealsByName">Search</button>
             </div>
         </form>
     </header>
 
     <section class="lg:container mx-auto mt-7 p-4">
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-6">
-            
-            <div v-for="meal in meals" :key="meal.idMeal" class="max-w-sm mx-auto md:mx-0 bg-white border border-gray-200 rounded-lg shadow">
-                <a href="#">
-                    <img class="rounded-t-lg" :src="meal.strMealThumb" :alt="meal.strMeal" />
-                </a>
-                <div class="p-5">
-                    <a href="#">
-                        <h5 class="maxLine-2 mb-2 text-2xl font-bold tracking-tight text-gray-900">{{ meal.strMeal }}</h5>
-                    </a>
-                    <p class="maxLines-3 mb-3 font-normal text-gray-700">{{ meal.strInstructions }}</p>
-                    <router-link to="" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
-                        Read more
-                        <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                        </svg>
-                    </router-link>
-                </div>
-            </div>
-
+            <MealsCard 
+                v-for="meal in meals" :key="meal.idMeal"
+                :title="meal.strMeal"
+                :discription="meal.strInstructions"
+                :image="meal.strMealThumb"
+            />
         </div>
     </section>
 </template>
 
 <script setup>
-    import { computed, onMounted, ref, watch } from 'vue';
+    import { computed, onMounted, ref } from 'vue';
     import { useStore } from 'vuex';
+    import MealsCard from '../components/mealsCard.vue';
 
     const mealByname = ref('');
     const store = useStore();
     const meals = computed(() => store.state.meals)
 
-
-
     const searchMealsByName = () => {
         store.dispatch('searchMealsByName', mealByname.value)
+
     }
 
     onMounted((
