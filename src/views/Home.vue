a31fdef50197696e50ba1e2ac174d3ec53c4af57
YerrouHamza <hamzayerrou5@yahoo.com>
2023-09-11 23:23:42 +0100

    small home fixed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -29,10 +29,11 @@
 </template>
 
 <script setup>
-    import MealsCard from '../components/mealscard.vue';
     import { computed, onMounted, ref } from 'vue';
     import { useStore } from 'vuex';
-
+    
+    import MealsCard from '../components/Mealscard.vue';
+    
     const mealByname = ref('');
     const store = useStore();
     const meals = computed(() => store.state.meals)
