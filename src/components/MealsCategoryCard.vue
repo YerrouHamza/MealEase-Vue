502b76f1476168278ba246061fbc0e6b6c82aea7
YerrouHamza <hamzayerrou5@yahoo.com>
2023-09-12 19:47:09 +0100

    make the meals category forwerd to all meals

--- src/components/MealsCategoryCard.vue
+++ src/components/MealsCategoryCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <router-link to="" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
+    <router-link :to="{name: 'alleMeals', params: {id: props.category}}" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
         <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" :src="props.image" :alt="props.category">
         <div class="flex flex-col justify-between p-4 leading-normal">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{{ props.category }}</h5>
