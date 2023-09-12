780f83c5e7df2c37933176a4b634311995f2cbd7
YerrouHamza <hamzayerrou5@yahoo.com>
2023-09-12 19:14:44 +0100

    creatd router and view page for all the meals

--- src/router/index.js
+++ src/router/index.js
@@ -2,21 +2,16 @@ import { createRouter, createWebHistory } from "vue-router";
 
 // Import the views components
 import Home from '../views/Home.vue';
-import Category from '../views/Category.vue'
-import Ingredient from '../views/Ingredient.vue'
-import SingleMeal from '../views/SingleMeal.vue'
+import Category from '../views/Category.vue';
+import Ingredient from '../views/Ingredient.vue';
+import SingleMeal from '../views/SingleMeal.vue';
+import AllMeals from '../views/AllMeals.vue';
 
 const routes = [
     {
         path: '/',
         name: 'home',
         component: Home
-    },
-
-    {
-        path: '/meal/:id?',
-        name: 'singleMeal',
-        component: SingleMeal
     },
 
     {
@@ -29,7 +24,19 @@ const routes = [
         path: '/category',
         name: 'category',
         component: Category
-    }
+    },
+
+    {
+        path: '/meal/:id?',
+        name: 'singleMeal',
+        component: SingleMeal,
+    },
+
+    {
+        path: '/allMeals/:id?',
+        name: 'alleMeals',
+        component: AllMeals,
+    },
 ]
 
 const router = createRouter({
---
+++ src/views/AllMeals.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>all meals</h1>
+
+</template>
+
+<script setup>
+    
+    
+</script>
+
+<style scoped>
+</style>
