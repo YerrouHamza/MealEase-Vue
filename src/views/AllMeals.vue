376b71c9e6005e562fcab16fc266251a6d5a1a98
YerrouHamza <hamzayerrou5@yahoo.com>
2023-09-12 21:32:26 +0100

    created the go back button

--- src/views/AllMeals.vue
+++ src/views/AllMeals.vue
@@ -1,5 +1,11 @@
 <template>
-    
+    <section class="lg:container mx-auto mt-10">
+        <router-link :to="{name: 'category'}" class="font-medium hover:text-blue-800 inline items-center text-xl text-blue-500 text-center">
+            <svg class="h h-4 inline w-6 mb-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13"></path>
+            </svg>
+            Go Back
+        </router-link>
+    </section>
     <section class="lg:container mx-auto mt-10 p-4">
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-6">
             <MealsCard 
