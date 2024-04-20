db26e3fc0b8b9da4626dae0be1a35c214d5f8f0f
hamzayerrou <hamzayerrou@groovetech.io>
2024-04-20 17:32:44 +0100

    created the notfound component

---
+++ src/components/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+    <section class="mb-10 flex flex-col justify-center items-center">
+        <img class="w-[300px]" :src="notFound" alt="not found data">
+        <h1 class="text-3xl text-center mb-2 font-bold uppercase">Sorry!!</h1>
+        <p class="text-xl capitalize">We haven't found any {{ title || 'Meals' }}</p>
+    </section>
+</template>
+
+<script setup>
+    import notFound from '../assets/images/notfound.gif';
+</script>
