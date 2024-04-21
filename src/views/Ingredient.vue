bb4e8ec64fef84842ef8c53473f461d0ee268362
yerrouhamza <hamzayerrou5@yahoo.com>
2024-04-21 17:10:51 +0100

    added the coming soon component to the ingredient page

--- src/views/Ingredient.vue
+++ src/views/Ingredient.vue
@@ -2,11 +2,12 @@
         <Header title="Ingredient"/>
         
         <section class="container max-w-screen-xl m-auto p-4 mt-10">
-            <h1>Coming Soon...</h1>
+            <ComingSoon />
         </section>
 </template>
 
 <script setup>
 import Header from '../components/Header.vue';
+import ComingSoon from '../components/ComingSoon.vue';
 
 </script>
